0189bf29ef15fd9d08cccf3acd24879b82c848d9
Ivanov Peter <petrivanov@gmail.com>
2023-01-30 15:13:04 +0300

    Some checkout, can be remove

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -3,6 +3,19 @@ import sqlite3
 import psycopg2
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
+from contextlib import contextmanager
+
+db_path = 'db.sqlite'
+
+
+@contextmanager
+def conn_context(db_parh: str):
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    yield conn
+    conn.close()
+
+
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
@@ -11,10 +24,10 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     # sqlite_extractor = SQLiteExtractor(connection)
 
     # data = sqlite_extractor.extract_movies()
-    # postgres_saver.save_all_data(data)
-
-
-if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
-        load_from_sqlite(sqlite_conn, pg_conn)
+#     # postgres_saver.save_all_data(data)
+#
+#
+# if __name__ == '__main__':
+#     dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
+#     with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+#         load_from_sqlite(sqlite_conn, pg_conn)
